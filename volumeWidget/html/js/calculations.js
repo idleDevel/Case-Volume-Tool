98b9d1fc0b58f1c6a026e76b303bad17c739a946
idleDevel <20792330+idleDevel@users.noreply.github.com>
2023-06-11 20:53:43 -0500

    Added License Blocks
    To every relevant file

--- volumeWidget/html/js/calculations.js
+++ volumeWidget/html/js/calculations.js
@@ -1,3 +1,26 @@
+/*
+ * calculations.js
+ *
+ * This file performs the neccessary math to convert units and find case volume, as well as footprint.
+ * 
+ * Author: Jakob Edwards (Github: @idleDevel) (SFF Forum: @Biowarejak)
+ *
+ * License: GPL-3.0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const mm3_to_m3 = 1000000000;
 const mm3_to_liters = 1000000;
 const mm3_to_cm3 = 1000;
--- volumeWidget/volume.py
+++ volumeWidget/volume.py
@@ -1,4 +1,26 @@
-from __future__ import print_function
+"""
+<volume.py>
+
+This file is the main application of a Pywebview app. It provides api hooks for javascript code and generates json files and folders to locally store data for the user. 
+
+Author: Jakob Edwards (Github: @idleDevel) (SFF Forum: @Biowarejak)
+
+License: GPL-3.0
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import webview
 import os
 import json
